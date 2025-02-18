9985ef570de3cf39804273f8a65367bc1014044a
HariRao2001 <raoharikrishna09@gmail.com>
2025-02-18 13:03:44 +0530

    add the mainThread.js and workerThread.js files into the git

---
+++ mainThread.js
@@ -0,0 +1,22 @@
+const http = require("node:http");
+const { Worker } =  require("node:worker_threads");
+
+const server = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.end("Main Page");
+    }
+    else if(req.url === "/slow-page"){
+        const worker = new Worker("./workerThread.js");
+        worker.on("message", (j)=>{
+            console.log(j);
+            res.writeHead(200, { "content-type": "text/plain" });
+            res.end(`Slow Page ${j}`);
+        })
+       
+    }
+});
+
+server.listen(3100, ()=>{
+    console.log(`Server is running at 3100 port`);
+})
